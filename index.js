ab28783ecaf944ca184b8a67d187964c3b8859cb
Urtė Nistirenko <urte.nistirenko@gmail.com>
2022-09-07 20:08:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// const userInput = "tomato";
+// switch (userInput) {
+//   case "orange":
+//   case "banana":
+//   case "apple":
+//   case "peach":
+//     console.log("Fruit");
+//     break;
+//   case "tomato":
+//   case "potato":
+//     console.log("vegetable");
+// }
+
+// let weekDay = new Date().getDay();
+// switch (weekDay) {
+//   case 0:
+//     weekDay = "Pirmadienis";
+//     break;
+//   case 1:
+//     weekDay = "Antradienis";
+//     break;
+//   case 2:
+//     weekDay = "Treciadienis";
+//     break;
+//   case 3:
+//     weekDay = "Katvirtadienis";
+//     break;
+//   case 4:
+//     weekDay = "Penktadienis";
+//     break;
+//   case 5:
+//     weekDay = "Sestadienis";
+//     break;
+//   case 6:
+//     weekDay = "Sekmdienis";
+//     break;
+// }
+// console.log(weekDay);
+
+// const myName = "Urteeee";
+// myName.length > 5 ? console.log("loong") : console.log("shrt");
+
+// const clientAge = 30;
+// const legalAge = 18;
+// clientAge < 0 || clientAge > 120
+//   ? console.log("Noper")
+//   : clientAge < legalAge
+//   ? console.log("Try next year")
+//   : console.log("All good");
+
+// const phone = "iPhone";
+// const isIphoneUser = phone === "iPhone";
+// console.log(isIphoneUser);
+
